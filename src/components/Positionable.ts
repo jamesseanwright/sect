0ddbb98f2fd06bb2b230272da096ffbf6d67b7ad
James Wright <james@jamesswright.co.uk>
2018-03-02 11:19:20 +0000

    WIP definition of Positionable props

--- src/components/Positionable.ts
+++ src/components/Positionable.ts
@@ -1,3 +1,13 @@
 import { Component } from './Component';
 
-export class Positionable extends Component {}
+export class Positionable extends Component {
+    x: Number;
+    y: Number;
+
+    constructor(x: Number, y: Number) {
+        super();
+
+        this.x = x;
+        this.y = y;
+    }
+}
