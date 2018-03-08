2a3e424d9123a3dd9ec8ec3fd12f10bbfcf63430
James Wright <james@jamesswright.co.uk>
2018-03-08 10:29:57 +0000

    WIP ImageRenderable props

--- src/components/ImageRenderable.ts
+++ src/components/ImageRenderable.ts
@@ -2,5 +2,10 @@ import ImageRenderSystem from '../systems/ImageRenderSystem';
 import { Component } from './Component';
 
 export class ImageRenderable extends Component {
+    public image: HTMLImageElement; // TODO: encapsulate!!!
 
+    constructor(image: HTMLImageElement) {
+        super();
+        this.image = image;
+    }
 }
