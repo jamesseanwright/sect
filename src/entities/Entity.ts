a716423246026585aa5e477bd6afe86e3a4970e3
James Wright <james@jamesswright.co.uk>
2018-03-17 16:57:26 +0000

    WIP systems registry

--- src/entities/Entity.ts
+++ src/entities/Entity.ts
@@ -1,4 +1,15 @@
 import { Component } from '../components';
+import System from '../systems/System';
+
+// WIP code to prototype component registration
+const systemsRegistry = new Map<string, System<Component>>();
+
+const createEntity = (...components: Component[]) => {
+    for (const component of components) {
+        const system = systemsRegistry.get(component.constructor.name);
+        system.register(component);
+    }
+};
 
 class Entity {
     public static create(...components: Component[]): Entity {
@@ -7,8 +18,7 @@ class Entity {
 
     private components: Map<string, Component>;
 
-    // TODO: test
-    protected constructor(components: Component[]) {
+    private constructor(components: Component[]) {
         this.components = new Map<string, Component>(
             components.map(c => [c.constructor.name, c] as [string, Component]),
         );
