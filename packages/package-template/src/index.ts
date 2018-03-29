e23eebe4725fcb8c7fbc729fdd276ac21c8387cd
James Wright <james@jamesswright.co.uk>
2018-03-29 18:07:30 +0100

    Adds basic package template

---
+++ packages/package-template/src/index.ts
@@ -0,0 +1,6 @@
+export { default as Component } from './Component';
+export { default as Entity } from './Entity';
+export { default as createEntityBinder } from './entityBinder';
+export { default as Game } from './Game';
+export { default as System } from './System';
+export { default as SystemRegistry } from './SystemRegistry';
