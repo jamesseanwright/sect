f00f3941e1fc6ac3cdbcba2aa110d03515c6c388
James Wright <james@jamesswright.co.uk>
2018-03-30 19:18:31 +0100

    WIP example game

--- packages/example-game/src/index.ts
+++ packages/example-game/src/index.ts
@@ -1,1 +1,5 @@
-export default () => undefined;
+const canvas: HTMLCanvasElement = document.body.querySelector('#game-output');
+const context = canvas.getContext('2d');
+
+context.fillStyle = 'red';
+context.fillRect(0, 0, 800, 600);
