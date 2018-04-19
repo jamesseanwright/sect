c7ee4f34733305c66c2715d3be1019dd6a52460b
James Wright <james@jamesswright.co.uk>
2018-04-19 17:05:28 +0100

    Removes old source folder

--- __src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// module that re-exports components for convenience
-
-export { default as AutoMoveable } from './AutoMoveable';
-export { default as Component } from './Component';
-export { default as ImageRenderable } from './ImageRenderable';
-export { default as Positionable } from './Positionable';
