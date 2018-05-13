699314e1cd465da7a83954b2d554d122c6ae89c1
James Wright <james@jamesswright.co.uk>
2018-05-13 19:25:19 +0100

    WIP linear 2d rect collision

--- packages/collision/src/LinearCollidable.ts
+++ packages/collision/src/LinearCollidable.ts
@@ -1,14 +1,13 @@
 import { Positionable } from '@tecs/basics';
 import { Component } from '@tecs/core';
-import CollisionTarget from './CollisionTarget';
 
 const toTuple = (key, value) => [key, value] as [typeof key, typeof value];
 
 class LinearCollidable extends Component {
     private _positionable: Positionable;
-    private _collisionTargets: Map<CollisionTarget, boolean>;
+    private _collisionTargets: Map<LinearCollidable, boolean>;
 
-    constructor(positionable: Positionable, ...collisionTargets: CollisionTarget[]) {
+    constructor(positionable: Positionable, ...collisionTargets: LinearCollidable[]) {
         super();
         this._positionable = positionable;
         this._collisionTargets = new Map(collisionTargets.map(target => toTuple(target, false)));
@@ -18,12 +17,14 @@ class LinearCollidable extends Component {
         return this._positionable;
     }
 
-    public get collisionTargets(): IterableIterator<CollisionTarget> {
+    public get collisionTargets(): IterableIterator<LinearCollidable> {
         return this._collisionTargets.keys();
     }
 
-    public updateCollision(target: CollisionTarget, isColliding: boolean) {
-        this._collisionTargets.set(target, isColliding);
+    public updateCollision(target: LinearCollidable, isColliding: boolean) {
+        if (this._collisionTargets.has(target)) {
+            this._collisionTargets.set(target, isColliding);
+        }
     }
 }
 
