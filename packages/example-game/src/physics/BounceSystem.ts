20e0873ba589926298953485fde85343a67476ac
James Wright <james@jamesswright.co.uk>
2018-05-16 17:09:12 +0100

    Removes redundant comment

--- packages/example-game/src/physics/BounceSystem.ts
+++ packages/example-game/src/physics/BounceSystem.ts
@@ -1,9 +1,3 @@
-/* This system is very much a WIP. Because
- * collision logic can differ so greatly between
- * games, I'm currently specifying everything in
- * this one system and a means of determining which
- * parts can be exposed by the collision package */
-
 import { System } from '@tecs/core';
 import { hasRectangularCollision, LinearCollidable, LinearCollisionSystem } from '@tecs/collision';
 import { Positionable, RectPositionable } from '@tecs/basics';
