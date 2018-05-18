04f5f17293f6efe1a3ae2f2a4e94c4bedbb62e86
James Wright <james@jamesswright.co.uk>
2018-05-18 18:51:56 +0100

    Adds WIP tests for rectangular collision

---
+++ packages/collision/src/collisions/__tests__/rectangular.test.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import { RectPositionable } from '@tecs/basics';
+import hasRectangularCollision from '../rectangular';
+
+describe('hasRectangularCollision', function () {
+    it('should return true when two RectPositionables are colliding', function () {
+        const aPositionable = new RectPositionable(10, 10, 50, 50);
+        const bPositionable = new RectPositionable(40, 30, 50, 50);
+        const hasCollision = hasRectangularCollision(aPositionable, bPositionable);
+
+        expect(hasCollision).to.be.true;
+    });
+
+    it('should return false when two RectPositionables are not colliding', function () {
+        const aPositionable = new RectPositionable(10, 10, 50, 50);
+        const bPositionable = new RectPositionable(51, 51, 50, 50);
+        const hasCollision = hasRectangularCollision(aPositionable, bPositionable);
+
+        expect(hasCollision).to.be.false;
+    });
+});
--- packages/collision/src/collisions/rectangular.ts
+++ packages/collision/src/collisions/rectangular.ts
@@ -1,7 +1,11 @@
 import { RectPositionable } from '@tecs/basics';
 
-const hasRectangularCollision = (a: RectPositionable, b: RectPositionable) => (
+const isWithinBounds = (a: RectPositionable, b: RectPositionable) => (
     a.x >= b.x && a.x <= b.x + b.width && a.y >= b.y && a.y <= b.y + b.height
+);
+
+const hasRectangularCollision = (a: RectPositionable, b: RectPositionable) => (
+    isWithinBounds(a, b) || isWithinBounds(b, a)
 );
 
 export default hasRectangularCollision;
