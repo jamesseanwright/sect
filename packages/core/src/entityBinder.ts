cf7d4f7ca3a416c7997dc25f4e06b1c7f8b9ab61
James Wright <james@jamesswright.co.uk>
2018-05-22 18:21:59 +0100

    Adds TODO for potential EntityBinder replacement

--- packages/core/src/entityBinder.ts
+++ packages/core/src/entityBinder.ts
@@ -5,6 +5,7 @@ import SystemRegistry from './SystemRegistry';
 
 export type EntityBinder = (entity: Entity) => Entity;
 
+// TODO: investigate putting this logic in Entity class and remove EntityBinder
 const createEntityBinder = (registry: SystemRegistry): EntityBinder => (entity: Entity): Entity => {
     for (const component of entity.components) {
         const system = registry.get(component.constructor);
