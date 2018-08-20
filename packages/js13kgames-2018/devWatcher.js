2d34f476df6271479ffcc975f14426a1308f7209
James Wright <james.wright@yld.io>
2018-08-20 09:59:51 +0100

    Adds watcher script with Observable

--- packages/js13kgames-2018/devWatcher.js
+++ packages/js13kgames-2018/devWatcher.js
@@ -2,13 +2,17 @@
 
 const path = require('path');
 const chokidar = require('chokidar');
+const child_process = require('child_process');
 const rollup = require('rollup');
 
 /* This allows us to determine if being run
  * at monorepo root or within this package */
 const currentPackageMetadata = require(path.join(process.cwd(), 'package.json'));
 const isRepoRoot = currentPackageMetadata.name === 'sect-monorepo';
-const watchPath = isRepoRoot ? 'packages' : '..';
+
+if (!isRepoRoot) {
+    throw new Error('Please run from the monorepo`s root!');
+}
 
 const createObservableWatcher = watch => {
     const createOperationState = (name, parameters) => ({
@@ -19,18 +23,31 @@ const createObservableWatcher = watch => {
     return {
         subscribe(eventName) {
             const createInvoker = operators => path => {
+                let value = path;
+
                 for (let { name, parameters } of operators.chain) {
                     switch(name) {
                         case 'filter':
-                            if (!parameters.predicate(path)) {
+                            if (!parameters.predicate(value)) {
                                 return;
                             }
 
                             break
 
                         case 'do':
-                            parameters.callback(path);
+                            parameters.callback(value);
                             break;
+
+                        case 'map':
+                            value = parameters.callback(value);
+                            break;
+
+                        case 'log':
+                            console.log(parameters.callback(path));
+                            break;
+
+                        default:
+                            throw new Error('Operator not found');
                     }
                 }
             };
@@ -46,6 +63,12 @@ const createObservableWatcher = watch => {
                     return this;
                 },
 
+                map(callback) {
+                    this.chain.push(
+                        createOperationState('map', { callback })
+                    );
+                },
+
                 do(callback) {
                     this.chain.push(
                         createOperationState('do', { callback })
@@ -53,6 +76,14 @@ const createObservableWatcher = watch => {
 
                     return this;
                 },
+
+                log(callback) {
+                    this.chain.push(
+                        createOperationState('log', { callback })
+                    );
+
+                    return this;
+                }
             };
 
             watch.on(eventName, createInvoker(operators));
@@ -62,7 +93,33 @@ const createObservableWatcher = watch => {
     };
 };
 
-createObservableWatcher(chokidar.watch(watchPath))
+console.log('')
+
+createObservableWatcher(chokidar.watch('packages'))
     .subscribe('change')
-    .filter(path => !path.includes('node_modules'))
-    .do(path => console.log(path))
+    .filter(path => !path.includes('node_modules') && !path.includes('js13kgames-2018'))
+    .log(path => `Change detected for ${path}`)
+    .map(path => /(\.\.\/.*)\/src/.exec(path)[1])
+    .log(path => `Rebuilding ${path}`)
+    .do(packageRoot => {
+        child_process.execSync('npm run build', {
+            cwd: packageRoot,
+            stdio: [
+                null,
+                process.stdout,
+                process.stderr,
+            ],
+        });
+    })
+    .log(path => `Built! ${path}. Rebuilding game...`)
+    .do(() => {
+        child_process.execSync('npm run build', {
+            cwd: __dirname,
+            stdio: [
+                null,
+                process.stdout,
+                process.stderr,
+            ],
+        });
+    })
+    .log(path => `Game built!`);
